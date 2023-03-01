ec9f53c7fda7d7eea172e2ae7adba0bef740cd31
seongjun.oh <tiger8708@gmail.com>
2023-03-02 05:36:25 +0900

    Typography에 numberOfLines prop 추가

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -5,17 +5,20 @@ export const Typography = ({
     color,
     fontSize,
     children,
+    numberOfLines,
 }: {
     color?: string;
     fontSize?: number;
     children: React.ReactNode | string;
+    numberOfLines?: number;
 }) => {
     return (
         <Text
             style={{
                 color: color,
                 fontSize: fontSize,
-            }}>
+            }}
+            numberOfLines={numberOfLines}>
             {children}
         </Text>
     );
